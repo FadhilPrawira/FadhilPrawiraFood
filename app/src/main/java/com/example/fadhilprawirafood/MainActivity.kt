82cc72c7adc0d382d322ddabe2f4d47e9fd320db
Fadhil Prawira <fadhilprawira87@gmail.com>
2024-07-28 23:49:51 +0700

    Slicing Figma to XML

---
+++ app/src/main/java/com/example/fadhilprawirafood/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.fadhilprawirafood
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnGetStarted = findViewById<AppCompatButton>(R.id.btnGetStarted);
+        btnGetStarted.setOnClickListener(
+            {
+                val HomePage = Intent(this, HomeActivity::class.java)
+                startActivity(HomePage)
+                finish()
+            },
+        )
+    }
+}
